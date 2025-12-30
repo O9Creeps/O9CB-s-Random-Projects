4a3e59b5f37c2924fa18d3f28aea2a50bf6b46fa
🏳‍🌈 O9Creeps 🐯 <zslaba8949@inst.hcpss.org>
2025-12-30 01:29:29 +0000

    no

---
+++ darkstalker.py
@@ -0,0 +1,10 @@
+# hi im darkstalker the nightwing and im going to KILL EVERYONE MUWAHAHAHA
+
+# ok so apparently pythone dont know about dargons so im going to include them as objects
+
+class Dragon:
+    def __init__(self, type, name):
+        self.name = name
+        self.type = type
+
+# fuck it im taking this somewhere else
