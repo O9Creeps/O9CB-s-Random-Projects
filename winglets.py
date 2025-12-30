3e36504379cf59e006921982a059d2ea02a5531e
🏳‍🌈 O9Creeps 🐯 <zslaba8949@inst.hcpss.org>
2025-12-30 00:04:54 +0000

    winglets.pie

---
+++ winglets.py
@@ -0,0 +1,27 @@
+# lets see how much the ai really knows about the winglets in WOF arc 2.
+
+jade_winglets = ["Tsunami", "Kinkajou", "Turtle", "Sunny"]  # correct
+lunar_winglets = ["Moonwatcher", "Winter", "Qibli", "Peril"]  # correct
+night_winglets = ["Starflight", "Darkstalker", "Fatespeaker"]
+# Darkstalker and Fatespeaker were never winglets
+# NightWings as a tribe do not have winglets
+skywing_winglets = ["Clay", "Sundew", "Blaze", "Carnelian"]  # correct
+sandwing_winglets = ["Flame", "Sandy", "Molly", "Dune"]  # Molly is not a sandwing
+seawing_winglets = ["Coral", "Anemone", "Indigo", "Riptide"]  # correct
+# Riptide is not a seawing
+darkstalker_winglets = ["Darkstalker", "Fatespeaker", "Clearsight"]  # Darkstalker and Fatespeaker were never winglets
+# Clearsight was not a winglet either
+# The AI seems to have a lot of inaccuracies regarding the winglets in WOF arc 2.
+
+"""
+these are the REAL winglets:
+"""
+
+jade = ["Winter", "Moon", "Kinkajou", "Turtle", "Carnelian", "Qibli", "Umber"]
+gold = ["Icicle", "Sora", "Bigtail", "Tamarin", "Onyx", "Pike", "Flame"]
+silver = ["Changbai", "Sepia", "Fearless", "Boto", "Ostrich", "Anemone", "Thrush"]
+copper = ["Alba", "Marsh", "Mindreader", "Coconut", "Pronghorn", "Snail", "Peregrine"]
+quartz = ["Ermine", "Newt", "Mightyclaws", "Siamang", "Arid", "Barracuda", "Garnet"]
+
+# i dont think the ai understands winglets very well
+
