9c8e96b6e603e4e82750b96645d33d9bb8049ea6
🏳‍🌈 O9Creeps 🐯 <zslaba8949@inst.hcpss.org>
2026-03-09 21:14:49 +0000

    dream is very homeless.

---
+++ dream.py
@@ -0,0 +1,17 @@
+import inspect
+
+class player:
+    def __init__(self, homeStatus="Has a Home"):
+        self.homeStatus = homeStatus
+    def die(self):
+        frame = inspect.currentframe().f_back
+        keys_to_delete = [name for name, val in frame.f_locals.items() if val is self]
+        
+        for name in keys_to_delete:
+            del frame.f_locals[name]
+
+dream = player("homeless")
+
+if dream.homeStatus == "homeless":
+    print("oh no")
+    dream.die()
